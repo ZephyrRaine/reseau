57dd7c682cafb3d6fb60e2d5befeca543a723926
ZephyrRaine <zephyr.raine@gmail.com>
2017-10-06 19:20:36 +0200

    OLOLOL

--- client.py
+++ client.py
@@ -2,23 +2,17 @@ import socket
 import sys
 
 # Create a TCP/IP socket
+waitingFor = True
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-address = ("133.133.2.66", 50000)
-sock.connect(address)
+address = ("localhost", 50000)
 
-try: 
-    message = b'Lol tg'
+while sock.connect_ex(address)!=0:
+    print("Waiting for server")
+
+print("Successfully connected to ", address)
+
+while True:
+    message = input("Write message: ")
+    
     print(message)
-    sock.sendall(message)
-
-    amount_received = 0
-    amount_expected = len(message)
-
-    while amount_received < amount_expected:
-        data = sock.recv(64)
-        amount_received += len(data)
-        print('received {!r}'.format(data))
-
-finally:
-    print('closing socket')
-    sock.close()
+    sock.sendall(message.encode())
--- server.py
+++ server.py
@@ -1,26 +1,56 @@
 import socket
 import sys
+import threading
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-address = ("localhost", 44444)
-sock.bind(address)
-sock.listen(1)
+class Server: # Définition de notre classe Personne
+
+    def __init__(self): # Notre méthode constructeur
+        self.users = list()
+        self.sockets = list()
+        self.userCount = 0
+        
+    def addClient(self, pseudo, client):
+        self.users.append(pseudo)
+        self.sockets.append(client)
+        self.userCount+=1
+
+    def chat(self):
+        if len(self.users) != 0:
+            print(str(len(self.sockets)) + " personnes")
+            for i in range(0, len(self.sockets)):
+                print("USER " + str(i))
+                try:
+                    data = self.sockets[i].recv(64)
+                    print(data.decode())
+                # if data:
+                #     pseudo = self.users[i]
+                #     msg = data.decode()
+                #     new = pseudo + " : " + msg 
+                #     for j in len(self.users):
+                #         self.sockets[j].send(new.encode())
+            
+
+class Listener(threading.Thread):
+
+    def __init__(self, serv):
+        self.serv = serv
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        address = ("localhost", 50000)
+        self.sock.bind(address)
+        self.sock.listen(1)
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            print("Waiting for client")
+            connection, client_address = self.sock.accept()
+            print("Connection from ", client_address)
+            connection.setblocking(0)
+            self.serv.addClient("", connection)
+
+server = Server()
+listener = Listener(server)
+listener.start()
 
 while True:
-    print("waiting")
-    connection, client_address = sock.accept()
-    try:
-        print("connection from ", client_address)
-        while True:
-            data = connection.recv(64)
-            print(format(data))
-            if data:
-                print('sending data back to the client')
-                connection.sendall(data)
-            else:
-                print('no data from', client_address)
-                break
-
-    finally:
-        connection.close()
+    server.chat()
